20594b322cf18f72b678559cc8bc4d3b67f4662f
abondar <aleksand.bondar.1985@gmail.com>
2024-01-31 18:22:02 +0200

    readme modified

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 	"github.com/bondar-aleksandr/cisco_parser"
 )
 
 var (
 	InfoLogger  *log.Logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	WarnLogger *log.Logger = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	ErrorLogger *log.Logger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 )
 
@@ -19,50 +21,110 @@ func main() {
 	start := time.Now()
 	InfoLogger.Println("Starting...")
 
-	var iFileName = flag.String("i", "", "input configuration file to parse data from")
-	var oFileName = flag.String("o", "", "output csv file, default is input filename with .csv extension")
-	var devtype = flag.String("t", "ios", "cisco OS family, possible values are ios, nxos. Default is ios")
-	var jsonOut = flag.Bool("j", false, "Whether JSON file needed. Default is false")
+	var firstFileName = flag.String("d1", "", "input configuration file to parse data from")
+	var firstPlatform = flag.String("p1", "ios", "cisco OS family, possible values are 'ios', 'nxos'. Default is 'ios'")
+	var secondFileName = flag.String("d2", "", "input configuration file to parse data from")
+	var secondPlatform = flag.String("p2", "ios", "cisco OS family, possible values are 'ios', 'nxos'. Default is 'ios'")
+	var action = flag.String("a", "parse", "action to perform. Possible values are 'parse', 'subnets'. Default is 'parse'" )
+	var format = flag.String("f", "csv", "output format, possible values are 'csv', 'json'. Default is csv")
 
 	if len(os.Args) < 2 {
 		ErrorLogger.Fatalf("No input data provided, use -h flag for help. Exiting...")
 	}
 	flag.Parse()
 
-	InfoLogger.Printf("Program started, got the following parameters:\ninput file: %s\noutput file: %s\ndevice type: %s\nJSON output: %v\n", *iFileName, *oFileName, *devtype, *jsonOut)
-
-	iFile, err := os.Open(*iFileName)
+	// read 1st device config
+	iFile, err := os.Open(*firstFileName)
 	if err != nil {
-		ErrorLogger.Fatalf("Can not open file %q because of: %q", *iFileName, err)
+		ErrorLogger.Fatalf("Can not open file %q because of: %q", *firstFileName, err)
 	}
 	defer iFile.Close()
 
-	interface_map, err := cisco_parser.ParseInterfaces(iFile, *devtype)
+	// create 1st device
+	device1, err := cisco_parser.NewDevice(iFile, *firstPlatform)
 	if err != nil {
-		ErrorLogger.Fatal(err)
-	}	
-
-	if *oFileName == "" {
-		*oFileName = cisco_parser.FileExtReplace(*iFileName, "csv")
+		ErrorLogger.Fatal("Cannot create 1st device:", err)
 	}
 
-	oFile, err := os.Create(*oFileName)
-	if err != nil {
-		ErrorLogger.Fatalf("Error in writing csv data to file %q because of: %q", oFile.Name(), err)
-	}
-	defer oFile.Close()
-	interface_map.ToCSV(oFile)
-	InfoLogger.Printf("Saved to %q\n", oFile.Name())
+	if *action == "parse" {
+		// prepare output file
 
-	if *jsonOut {		
-		jsonFileName := cisco_parser.FileExtReplace(*oFileName, "json")
-		jsonFile, err := os.Create(jsonFileName)
+		oFileName := FileExtReplace(*firstFileName, *format)
+		
+		oFile, err := os.Create(oFileName)
 		if err != nil {
-			ErrorLogger.Fatalf("Error in writing json data to file %q because of: %q", jsonFile.Name(), err)
+			ErrorLogger.Fatalf("can't create output file: %q", err)
 		}
-		defer jsonFile.Close()
-		interface_map.ToJSON(jsonFile)
-		InfoLogger.Printf("Saved to %q\n", jsonFileName)
+		defer oFile.Close()
+	
+		// prepare serializer
+		serializer, err := cisco_parser.NewSerializer(oFile, device1, *format)
+		if err != nil {
+			ErrorLogger.Fatal(err)
+		}
+		// serialize 
+		if err = serializer.Serialize(); err != nil {
+			ErrorLogger.Fatal(err)
+		}
+		InfoLogger.Printf("saved as %s\n", oFileName)
+
+	} else if *action == "subnets" {
+		// check whether all required flags provided
+		if *secondFileName == "" {
+			ErrorLogger.Fatal("not enough arguments provided for 2nd device!")
+		}
+		// read 2nd device config
+		iFile, err := os.Open(*secondFileName)
+		if err != nil {
+			ErrorLogger.Fatalf("Can not open file %q because of: %q", *secondFileName, err)
+		}
+		defer iFile.Close()
+	
+		// create 2тв device
+		device2, err := cisco_parser.NewDevice(iFile, *secondPlatform)
+		if err != nil {
+			ErrorLogger.Fatal("Cannot create 2nd device:", err)
+		}
+		GetCommonSubnets(device1, device2)
+	} else {
+		fmt.Println("Wrong action specified. Exiting...")
 	}
 	InfoLogger.Printf("Finished! Time taken: %s\n", time.Since(start))
+}
+
+// GetCommonSubnets finds subnets in common for 2 devices.
+func GetCommonSubnets(d1, d2 *cisco_parser.Device) {
+	dev1Subnets, err := d1.GetSubnets()
+	if err != nil {
+		ErrorLogger.Fatal(err)
+	}
+	var counter uint16
+	for _, p := range dev1Subnets {
+		d2Data, err := d2.GetSubnetData(p)
+		if err != nil {
+			ErrorLogger.Fatal("Cannot get 2nd device subnets data:", err)
+		}
+		// if no matching subnet found on d2
+		if d2Data == nil {
+			continue
+		} else {
+			counter++
+			d1Data,_ := d1.GetSubnetData(p)		// get d1 subnet data as well
+			fmt.Printf("\t%s\n", p)
+			fmt.Println("--------")
+			fmt.Printf("%q device interfaces:\n", d1.Hostname)
+			fmt.Println(d1Data)
+			fmt.Println("--------")
+			fmt.Printf("%q device interfaces:\n", d2.Hostname)
+			fmt.Println(d2Data)
+			fmt.Println("--------")
+		}
+	}
+	InfoLogger.Printf("Found %d common subnets\n", counter)
+}
+
+// utility function for file extension replacement.
+func FileExtReplace(f string, ex string) string {
+	bareName := strings.TrimSuffix(f, filepath.Ext(f))
+	return fmt.Sprintf("%s.%s", bareName, ex)
 }
--- readme.md
+++ readme.md
@@ -1,12 +1,29 @@
 # Cisco config parser CLI
 
-This app is a CLI version of [https://github.com/bondar-aleksandr/cisco_parser](https://github.com/bondar-aleksandr/cisco_parser)
+This app is aimed to parse Cisco devices config into structured format (csv or json).
+Supported device types: IOS/IOS-XE/IOS-XR/NXOS.
+We can further convert .csv result file to excel file for convenient analysis.
+It's also possible to search for common subnets between two devices, might be useful for drawing logical network topology.
+App uses [https://github.com/bondar-aleksandr/cisco_parser](https://github.com/bondar-aleksandr/cisco_parser) as a library.
 ___
 ## Usage
 
-Upon startup app parses cli arguments in order to determine cisco config-file location, output location, and output format. As a result, we got .csv file with device's interfaces, which may be used later for analysis (import to excell for example). Also it's possible to get .josn formatted output as well.
+CLI flags description is below:
 
-*Supported device types*: IOS/IOS-XE/IOS-XR/NXOS
+
+| Flag | Data Type | Mandatory | Description | 
+| ------ | ----------- | --- | --|
+| -d1 | string | yes | cisco config-file location (1st device) |
+| -p1 | string | yes | 1st device platform (**ios/nxos**) |
+| -d2 | string | no | cisco config-file location (2nd device) |
+| -p2 | string | no | 2nd device platform (**ios/nxos**) |
+| -a | string | no | action to perform. Possible values are **parse, subnets**. Default is **parse** |
+| -f | string | no | output format. Default is **csv**. Possible values are **csv, json** |
+| -h | -- | no | get help |
+
+Upon startup app parses cli arguments in order to determine cisco config-file location(s), output data format, and action needed to be performed.
+
+### Prasing
 
 Currently, the following interface values are parsed:
 - name
@@ -19,26 +36,15 @@ Currently, the following interface values are parsed:
 - input ACL
 - output ACL
 
-CLI flags description is below:
+Device hostname parsed as well
 
-
-| Flag | Data Type | Mandatory | Description | 
-| ------ | ----------- | --- | --|
-| -i | string | yes | Input cisco config-file location |
-| -o | string | no | Output file location. Default is the same as input file, but with replaced extention |
-| -t | string | no | OS type, possible values are "ios", "nxos". Default os "ios" |
-| -j | -- | no | Whether we need additional json output. Default is "false" |
-| -h | -- | no | Get help |
-
-Launch app example:
+Launch app for parsing:
 ```
-main.exe -i ciscoConfig.cfg
+app.exe -d1 ciscoConfig.cfg -p1 ios -f csv -a parse
 ```
 In this case we will get the .csv file name "ciscoConfig.csv" in current working directory
 
-___
-## Output data format
-Let's suppose, we have interface config as follow:
+Let's suppose we have the following interface configuration in config file:
 ```
 !
 interface GigabitEthernet0/0/2.2
@@ -58,3 +64,23 @@ Name,Description,Encapsulation,Ip_addr,Subnet,Vrf,Mtu,ACLin,ACLout
 GigabitEthernet0/0/2,TUNNEL-SOURCE_INET,dot1q 2,1.2.3.4/27,1.2.3.0/27,INET,,FROM_INET_IPSEC,
 ```
 
+### Common subnets search
+
+Launch app for parsing:
+```
+app.exe -d1 ciscoConfig01.cfg -p1 ios -d2 ciscoConfig02.cfg -p2 nxos -a subnets
+```
+App output will look like
+```
+        10.10.5.0/30
+--------
+"ciscoConfig01" device interfaces:
+interface: "Vlan20", vrf: ""
+
+--------
+"ciscoConfig02" device interfaces:
+interface: "Vlan20", vrf: "WAN"
+
+--------
+INFO: 2024/01/31 17:58:43 main.go:123: Found 1 common subnets
+```
